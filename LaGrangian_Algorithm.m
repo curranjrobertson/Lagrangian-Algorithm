6fb8c03a745d54d4f5b04060a4839fdaa34f4a4f
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-03-26 16:44:10 -0600

    update2

--- LaGrangian_Algorithm.m
+++ LaGrangian_Algorithm.m
@@ -3,7 +3,7 @@
 % March 21, 2023
 
 % Symbols
-syms x s fi t k
+syms x(t) s(t) fi(t) k
 
 % Constants
 g = 9.81;
@@ -12,27 +12,27 @@ g = 9.81;
 m1 = 200;
 m2 = 350;
 m3 = 0;
-q1 = x;
-q2 = fi;
-q3 = s;
+q1 = x(t);
+q2 = fi(t);
+q3 = s(t);
 
 % Kinetic Energy
-q1_dot = diff(q1, t)
-q2_dot = diff(q2, t)
-q3_dot = diff(q3, t)
+xDot = diff(x, t)
+fiDot = diff(q2, t)
+sDot = diff(q3, t)
 
-T1 = (m1/2)*(q1_dot^2 + q3_dot^2)
-T2 = (m2/2)*(q2_dot^2 + q2_dot^2)
-T3 = (m3/2)*(q3_dot^2 + q2_dot^2)
+T1 = (m1/2)*(xDot^2 + sDot^2)
+T2 = (m2/2)*(fiDot^2 + fiDot^2)
+T3 = (m3/2)*(sDot^2 + sDot^2)
 T = T1 + T2
 
 % Potential Energy
 V1_elastic = 0;
 V1_gravitational = 0;
-V2_elastic = (1/2)*k*s^2;
+V2_elastic = (1/2)*k*q3^2;
 V2_gravitational = 0;
 V3_elastic = 0;
-V3_gravitational = m2*g*cos(fi);
+V3_gravitational = m2*g*cos(q2);
 V1 = V1_elastic + V1_gravitational;
 V2 = V2_elastic + V2_gravitational;
 V3 = V3_elastic + V3_gravitational;
@@ -46,9 +46,9 @@ dL_dx = diff(L, x)
 dL_dfi = diff(L, fi)
 dL_ds = diff(L, s)
 
-dL_dx_dot = diff(L, )
-dL_dfi_dot = diff(L, q2_dot)
-dL_ds_dot = diff(L, q3_dot)
+dL_dx_dot = diff(L, xDot)
+dL_dfi_dot = diff(L, fiDot)
+dL_ds_dot = diff(L, sDot)
 
 % Time derivatives
 dL_dx_dot_dot = diff(dL_dx_dot, t);
@@ -56,9 +56,9 @@ dL_dfi_dot_dot = diff(dL_dfi_dot, t);
 dL_ds_dot_dot = diff(dL_ds_dot, t);
 
 % Lagrangian Equation of Motion
-q1_dot_dot = diff(q1, t, 2);
-q2_dot_dot = diff(q1, t, 2);
-q3_dot_dot = diff(q1, t, 2);
+xDotDot = diff(q1, t, 2)
+fiDotDot = diff(q2, t, 2)
+sDotDot = diff(q3, t, 2)
 
 eqn1 = dL_dx_dot_dot - dL_dx;
 eqn2 = dL_dfi_dot_dot - dL_dfi;
