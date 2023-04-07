d6705559d124cf48b5188db5928e747b651b2008
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-04-07 15:36:01 -0600

    remove old code

--- LaGrangian_Algorithm.m
+++ LaGrangian_Algorithm.m
@@ -8,7 +8,7 @@
 clear all; clc; close all;
 
 % Symbols
-syms dq1(t) dq2(t) m1 m2 g
+syms m1 m2 g
 
 q1(t) = 
 q2(t) = 
@@ -16,8 +16,8 @@ y1(t) =
 y2(t) = 
 
 % Kinetic Energy
-% dq1(t) = diff(q1(t), t)
-% dq2(t) = diff(q2(t), t)
+dq1(t) = diff(q1(t), t)
+dq2(t) = diff(q2(t), t)
 
 T1(t) = (m1/2)*(dq1(t)^2)
 T2(t) = (m2/2)*(dq2(t)^2)
@@ -39,7 +39,6 @@ D3(t) = functionalDerivative(L, dq1(t))
 D4(t) = functionalDerivative(L, dq2(t))
 
 % Time derivatives
-% Time derivatives
 D5(t) = diff(subs(D3(t),dq1,diff(q1,t)), t)
 D6(t) = diff(subs(D4(t),dq2,diff(q2,t)), t)
 
@@ -47,17 +46,3 @@ D6(t) = diff(subs(D4(t),dq2,diff(q2,t)), t)
 eqn = [simplify(D5 - D1); simplify(D6 - D2)] == 0
 assumeAlso([m1 m2],'positive')
 eqn = simplify(eqn)
-
-% D5(t) = functionalDerivative(D3(t), t)
-% D6(t) = functionalDerivative(D4(t), t)
-% 
-% % Lagrangian Equation of Motion
-% accel1 = functionalDerivative(q1, t, 2)
-% accel2 = functionalDerivative(q2, t, 2)
-
-% eqn1 = simplify(D5 - D1);
-% eqn2 = simplify(D6 - D2);
-
-% % Solutions
-% q1Accel = solve(eqn1, accel1);
-% q2Accel = solve(eqn2, accel2);
